e05268003cafb422cdeeeec3972c10cfc5a7a211
Nilesh Kumar <34126021+nileshkumar94@users.noreply.github.com>
2017-12-18 23:55:40 +0530

    get random quote everytime
    Generated a random number- "num".

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-    $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-});
+function getQuotes() {
+
+    var num = Math.round(new Date().getTime() / 1000);
+
+    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + num, function(a) {
+        $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
+    });
+
+}
+getQuotes();
