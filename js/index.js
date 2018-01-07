3d844da519d9b43784a5be77bc643d3883cbb69f
Nilesh Kumar <34126021+nileshkumar94@users.noreply.github.com>
2018-01-07 19:42:57 +0530

    for security replaced http by https

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ function getQuotes() {
 
     var num = Math.round(new Date().getTime() / 1000);
 
-    $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + num, function(a) {
+    $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=" + num, function(a) {
         $(".quote").html("<i class= 'fa fa-quote-left'></i>" + a[0].content + "<i class= 'fa fa-quote-right '></i><p class='author'><b>- " + a[0].title + "</b></p>")
     });
 
